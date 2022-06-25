9517be6d9e4dbabd39223d115c328203876cbc1f
zuranthus <7449346+zuranthus@users.noreply.github.com>
2022-06-25 18:20:27 +0200

    wip

--- src/main.zig
+++ src/main.zig
@@ -58,18 +58,25 @@ const ZwmErrors = error{
 const Manager = struct {
     d: *c.Display,
     r: c.Window,
+    clients: std.AutoHashMap(c.Window, c.Window),
 
     var ce: ?ZwmErrors = null;
 
-    pub fn Create(display: ?[]u8) !Manager {
+    pub fn init(display: ?[]u8) !Manager {
         _ = display;
         const d = c.XOpenDisplay(":1") orelse return ZwmErrors.Error;
         const r = c.XDefaultRootWindow(d);
-        return Manager{ .d = d, .r = r };
+        return Manager{
+            .d = d,
+            .r = r,
+            .clients = std.AutoHashMap(c.Window, c.Window).init(std.heap.c_allocator),
+        };
     }
 
-    pub fn Destroy(m: Manager) void {
+    pub fn deinit(m: *Manager) void {
         _ = c.XCloseDisplay(m.d);
+        // TODO deinit clients
+        m.clients.deinit();
         log("destroyed wm\n");
     }
 
@@ -82,7 +89,7 @@ const Manager = struct {
         log("initialized wm\n");
     }
 
-    fn _EventLoop(m: Manager) !void {
+    fn _EventLoop(m: *Manager) !void {
         while (true) {
             var e: c.XEvent = undefined;
             _ = c.XNextEvent(m.d, &e);
@@ -92,6 +99,7 @@ const Manager = struct {
                 c.CreateNotify => m._OnCreateNotify(e.xcreatewindow),
                 c.DestroyNotify => m._OnDestroyNotify(e.xdestroywindow),
                 c.ReparentNotify => m._OnReparentNotify(e.xreparent),
+                c.MapNotify => m._OnMapNotify(e.xmap),
                 c.ConfigureRequest => m._OnConfigureRequest(e.xconfigurerequest),
                 c.MapRequest => m._OnMapRequest(e.xmaprequest),
                 else => logf("ignored event {s}\n", .{ename}),
@@ -99,22 +107,21 @@ const Manager = struct {
         }
     }
 
-    fn _OnCreateNotify(m: Manager, ev: c.XCreateWindowEvent) !void {
+    fn _OnCreateNotify(m: *Manager, ev: c.XCreateWindowEvent) !void {
         _ = m;
         _ = ev;
     }
 
-    fn _OnDestroyNotify(m: Manager, ev: c.XDestroyWindowEvent) !void {
+    fn _OnDestroyNotify(m: *Manager, ev: c.XDestroyWindowEvent) !void {
         _ = m;
         _ = ev;
     }
 
-    fn _OnReparentNotify(m: Manager, ev: c.XReparentEvent) !void {
-        _ = m;
-        _ = ev;
-    }
+    fn _OnReparentNotify(_: *Manager, _: c.XReparentEvent) !void {}
 
-    fn _OnConfigureRequest(m: Manager, ev: c.XConfigureRequestEvent) !void {
+    fn _OnMapNotify(_: *Manager, _: c.XMapEvent) !void {}
+
+    fn _OnConfigureRequest(m: *Manager, ev: c.XConfigureRequestEvent) !void {
         var changes = c.XWindowChanges{
             .x = ev.x,
             .y = ev.y,
@@ -124,15 +131,48 @@ const Manager = struct {
             .sibling = ev.above,
             .stack_mode = ev.detail,
         };
-        _ = c.XConfigureWindow(m.d, ev.window, @intCast(c_uint, ev.value_mask), &changes);
-        logf("resize {} to ({}, {})\n", .{ ev.window, changes.width, changes.height });
+        var w = ev.window;
+        if (m.clients.get(w)) |frame| {
+            w = frame;
+            log("resizing frame:\n");
+        }
+        _ = c.XConfigureWindow(m.d, w, @intCast(c_uint, ev.value_mask), &changes);
+        logf("resize {} to ({}, {})\n", .{ w, changes.width, changes.height });
     }
 
-    fn _OnMapRequest(m: Manager, ev: c.XMapRequestEvent) !void {
+    fn frameWindow(m: *Manager, w: c.Window) !void {
+        const border_width = 3;
+        const border_color = 0xff0000;
+        const bg_color = 0x0000ff;
+
+        var wattr: c.XWindowAttributes = undefined;
+        if (c.XGetWindowAttributes(m.d, w, &wattr) == 0) return ZwmErrors.Error;
+
+        const frame = c.XCreateSimpleWindow(
+            m.d,
+            m.r,
+            wattr.x,
+            wattr.y,
+            @intCast(c_uint, wattr.width),
+            @intCast(c_uint, wattr.height),
+            border_width,
+            border_color,
+            bg_color,
+        );
+        _ = c.XSelectInput(m.d, frame, c.SubstructureRedirectMask | c.SubstructureNotifyMask);
+        _ = c.XAddToSaveSet(m.d, w);
+        _ = c.XReparentWindow(m.d, w, frame, 0, 0);
+        _ = c.XMapWindow(m.d, frame);
+        try m.clients.put(w, frame);
+        logf("framed window {} [{}]\n", .{ w, frame });
+    }
+
+    fn _OnMapRequest(m: *Manager, ev: c.XMapRequestEvent) !void {
+        try m.frameWindow(ev.window);
         _ = c.XMapWindow(m.d, ev.window);
     }
 
-    pub fn Run(m: Manager) !void {
+    pub fn Run(m: *Manager) !void {
         try m._InitWm();
         try m._EventLoop();
     }
@@ -155,8 +195,8 @@ const Manager = struct {
 pub fn main() !void {
     log("starting\n");
 
-    const m = try Manager.Create(null);
-    defer m.Destroy();
+    var m = try Manager.init(null);
+    defer m.deinit();
 
     try m.Run();
 
